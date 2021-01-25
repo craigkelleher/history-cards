9c36d865cd6f9b84c2fb8a7c766bc2072818172b
Craig Kelleher <32819654+craigkelleher@users.noreply.github.com>
2021-01-25 00:45:01 -0800

    Created gameboard, player/opponant areas, and Draw Cards functonality
    Created gameboard, player/opponant areas, and Draw Cards functonality

--- Assets/Scripts/drawCards.cs
+++ Assets/Scripts/drawCards.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Enable the Draw Cards button for the game
-public class drawCards : MonoBehaviour
+// Enable the Draw Cards button for the game
+public class DrawCards : MonoBehaviour
 {
     public GameObject Card1;
     public GameObject Card2;
@@ -15,9 +15,20 @@ public class drawCards : MonoBehaviour
 
     }
 
+    // Inspired by Unity Tutorial - Drag & Drop Tutorial
     public void OnClick()
     {
-        GameObject playerCard = Instantiate(Card1, new Vector3);
+        //if starting hand size if 5
+        for (var i = 0; i < 6; i++)
+        {
+            // Make new player card and set it to the instantiation of new card, create an instance of card 1 public game object
+            // Quarternion means no rotation on object at the moment
+            GameObject playerCard = Instantiate(Card1, new Vector3(0, 0, 0), Quaternion.identity);
+            // every game object in Unity has a property called transform, meaning where it should be/live, and we want to set its parent to be the player area transform
+            // when we instnatiate this player card object, we want to set it as a child as player area game object. 
+            playerCard.transform.SetParent(PlayerArea.transform, false);
+        }
+
     }
 
 }
